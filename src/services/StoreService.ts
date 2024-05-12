1333b633c705ce260211fd7bd0f12448bb0ed66a
daleksic13777 <daleksic@theflowerpress.net>
2024-05-12 20:36:48 +0200

    refactored

--- src/services/StoreService.ts
+++ src/services/StoreService.ts
@@ -2,7 +2,16 @@ import { Store } from "../entity/Store";
 // import { StoreMd } from "../entity/StoreMd";
 
 export const fillMetadatVariables = (metadata: any, store: Store, fullCountryName: string | undefined) => {
-    const couponLabel = store.coupons[0].label ? store.coupons[0].label : "";
+    let storeCouponsLength = store.coupons.length;
+    let couponLabel: string;
+
+    if (storeCouponsLength === 0) {
+        couponLabel = "";
+    } else {
+        let randomCoupon = getRandomIntBetween(0, storeCouponsLength - 1);
+        couponLabel = store.coupons[randomCoupon].label;
+    }
+
     const { fullYear, month } = getCurrenYearAndMonth();
     const currentWebsiteName = 'trusted.coupons'
 
@@ -11,25 +20,32 @@ export const fillMetadatVariables = (metadata: any, store: Store, fullCountryNam
         DEAL: couponLabel,
         CURRENT_MONTH: month,
         CURRENT_YEAR: fullYear,
-        STORE_CATEGORY: store.mainCategory, 
+        STORE_CATEGORY: store.mainCategory.replace(/_/g, " "),
         COUNTRY: fullCountryName,
         MY_WEBSITE: currentWebsiteName
+
     }
     for (const key in metadata) {
         for (const varName in replacements) {
             const varToReplace = `{{${varName}}}`;
             metadata[key].metadata_description = metadata[key].metadata_description.replace(new RegExp(varToReplace, 'g'), replacements[varName]);
             metadata[key].metadata_title = metadata[key].metadata_title.replace(new RegExp(varToReplace, 'g'), replacements[varName]);
-      
+
         }
     }
-  
+
     return metadata;
 }
 
-const getStoreName = (storeName:string):string =>{
-    const arrStr = storeName.split(".");
-    return arrStr.slice(1, -1).join(".");
+const getStoreName = (storeName: string): string => {
+    const dotIndex1 = storeName.indexOf(".");
+    const dotIndex2 = storeName.lastIndexOf(".");
+
+    if (dotIndex1 !== -1 && dotIndex2 !== -1 && dotIndex1 !== dotIndex2) {
+        return storeName.substring(dotIndex1 + 1, dotIndex2);
+    } else {
+        return storeName.substring(0, dotIndex2);
+    }
 }
 
 const getCurrenYearAndMonth = () => {
@@ -41,3 +57,10 @@ const getCurrenYearAndMonth = () => {
         month
     }
 }
+
+const getRandomIntBetween = (min: number, max: number) => {
+    if (min === max) {
+        return max;
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};

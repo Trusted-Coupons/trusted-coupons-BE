4de20cf9686ecd0c314a41f8ea9df5c30c40216a
Nikola Grujicic <nikolagrujicic@Nikolas-MacBook-Pro.local>
2024-05-14 14:19:01 +0200

    added changes for vercel

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,45 @@
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
-import { AppDataSource } from "./data-source"
-import { Routes } from "./routes"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import { Routes } from "./routes";
 
-AppDataSource.initialize().then(async () => {
-// Create a Redis client
+const PORT = process.env.PORT || 3010;
 
+AppDataSource.initialize()
+  .then(async () => {
+    // Create a Redis client
 
     // create express app
-    const app = express()
-    app.use(bodyParser.json())
+    const app = express();
+    app.use(bodyParser.json());
     // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: Function) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then((result) =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
+        }
+      );
+    });
 
     // setup express app here
     // ...
 
     // start express server
-    app.listen(3090)
-
-
-
-}).catch(error => console.log(error))
+    app.listen(PORT);
+  })
+  .catch((error) => console.log(error));

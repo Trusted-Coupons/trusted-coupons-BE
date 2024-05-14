139599bf6a8a35f8d6e02f35d563aa2175c94a45
daleksic13777 <daleksic@theflowerpress.net>
2024-05-14 12:55:24 +0200

    stores new table

--- src/entity/Store.ts
+++ src/entity/Store.ts
@@ -54,58 +54,6 @@ export class Store {
     @Column({ type: "varchar", length: 255 })
     country_language: string;
 
-
-    // @Column({ type: "varchar", length: 255 })
-    // store: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // icon: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // previewDesktop: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // previewMobile: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // description: string;
-
-    // @Column({ type: "integer" })
-    // globalRank: number;
-
-    // @Column({ type: "varchar", length: 2 })
-    // countryAlpha2Code: string;
-
-    // @Column({ type: "integer" })
-    // countryRank: number;
-
-    // @Column({ type: "integer" })
-    // categoryRank: number;
-
-    // @Column({ type: "integer" })
-    // monthlyVisits: number;
-
-    // @Column({ type: "varchar", length: 255 })
-    // mainCategory: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // altTopics: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // altCategories: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // keywords: string;
-
-    // @Column({ type: "integer" })
-    // totalCountries: number;
-
-    // @Column({ type: "varchar", length: 255 })
-    // countries: string;
-
-    // @Column({ type: "varchar", length: 255 })
-    // country_language: string;
-
     @Column({ type: "varchar", length: 255, name: 'AU_English' })
     AU_English: string;
 

8c93a9eb651695e163bf3c45cada6d3b36bef785
daleksic13777 <daleksic@theflowerpress.net>
2024-05-15 13:59:18 +0200

    port change

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,28 @@
-import "reflect-metadata";
-import { DataSource } from "typeorm";
-import { Coupon } from "./entity/Coupon";
-import { CountryLanguage } from "./entity/CountryLanguage";
-import { Store } from "./entity/Store";
-import { Category } from "./entity/Category";
-import { StoreMd } from "./entity/StoreMd";
-require("dotenv").config();
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { Coupon } from "./entity/Coupon" 
+import { CountryLanguage } from "./entity/CountryLanguage"
+import { Store} from "./entity/Store"
+import { Category } from "./entity/Category"
+import { StoreMd } from "./entity/StoreMd"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD?.toString(),
-    database: process.env.DATABASE,
+    host: "89.216.18.147",
+    port: 5432,
+    username: "postgres",
+    password: "Admin1",
+    database: "postgres",
     synchronize: false,
     logging: false,
-    entities: [Coupon, CountryLanguage, Store, Category, StoreMd],
+    entities: [
+        Coupon,
+        CountryLanguage,
+        Store,
+        Category,
+        StoreMd
+    ],
     migrations: [],
     migrationsRun: false,
     subscribers: [],
-});
+})

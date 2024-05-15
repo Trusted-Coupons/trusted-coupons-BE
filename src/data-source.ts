a9e75ee8ec64c008459283813efde0e78e063c9f
daleksic13777 <daleksic@theflowerpress.net>
2024-05-15 13:00:08 +0200

    added .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,28 +1,23 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Coupon } from "./entity/Coupon" 
-import { CountryLanguage } from "./entity/CountryLanguage"
-import { Store} from "./entity/Store"
-import { Category } from "./entity/Category"
-import { StoreMd } from "./entity/StoreMd"
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Coupon } from "./entity/Coupon";
+import { CountryLanguage } from "./entity/CountryLanguage";
+import { Store } from "./entity/Store";
+import { Category } from "./entity/Category";
+import { StoreMd } from "./entity/StoreMd";
+require("dotenv").config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "89.216.18.147",
-    port: 5432,
-    username: "postgres",
-    password: "Admin1",
-    database: "postgres",
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD?.toString(),
+    database: process.env.DATABASE,
     synchronize: false,
     logging: false,
-    entities: [
-        Coupon,
-        CountryLanguage,
-        Store,
-        Category,
-        StoreMd
-    ],
+    entities: [Coupon, CountryLanguage, Store, Category, StoreMd],
     migrations: [],
     migrationsRun: false,
     subscribers: [],
-})
+});
--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ AppDataSource.initialize().then(async () => {
     // ...
 
     // start express server
-    app.listen(3090)
+    app.listen(3060)
 
 
 

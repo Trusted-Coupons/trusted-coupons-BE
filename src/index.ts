70ce36dbd0ae58574aac738f54f6575286c97566
daleksic13777 <daleksic@theflowerpress.net>
2024-05-22 12:21:01 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express"
 import { AppDataSource } from "./data-source"
 import { Routes } from "./routes"
 const cors = require("cors");
+require('dotenv').config({ path:'.env' })
 
 AppDataSource.initialize().then(async () => {
     // Create a Redis client

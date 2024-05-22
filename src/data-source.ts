198fbe61d60ca25e130781227b0d38408787006c
daleksic13777 <daleksic@theflowerpress.net>
2024-05-22 12:25:36 +0200

    cleanup

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,15 +5,14 @@ import { CountryLanguage } from "./entity/CountryLanguage"
 import { Store} from "./entity/Store"
 import { Category } from "./entity/Category"
 import { StoreMd } from "./entity/StoreMd"
-require('dotenv').config({ path:'.env' })
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT as string, 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: "89.216.18.147",
+    port: 5432,
+    username: "postgres",
+    password: "Admin1",
+    database: "postgres",
     synchronize: false,
     logging: false,
     entities: [

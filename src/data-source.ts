8bde60ab7feef35d7a71a5214fea343ba32d2436
daleksic13777 <daleksic@theflowerpress.net>
2024-05-27 19:03:32 +0200

    change data source to read port from .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,7 +9,7 @@ require('dotenv').config({ path:'.env' })
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST,
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5432,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
     username:process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
